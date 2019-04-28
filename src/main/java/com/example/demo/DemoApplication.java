4f3a17faa95e129b9b2deabfc977d3184822ab70
aibaiyang <946452613@qq.com>
2019-04-29 00:23:29 +0800

    添加配置文件；

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,5 +9,6 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         //dev分支修改
+        // hello world
     }
 }
